9929f3785c881d81a57e1f53471ed724d2be767a
bogdan <bogdanarsaman@gmail.com>
2023-12-29 14:04:28 +0100

    made footer more rounded

--- monster-studio/app/footer.tsx
+++ monster-studio/app/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="w-full grid grid-cols-12 items-end">
-            <div className="bg-zinc-900 rounded-t-3xl col-start-2 col-span-10 lg:col-start-5 lg:col-span-4">
+            <div className="bg-zinc-900 rounded-t-[5rem] col-start-2 col-span-10 lg:col-start-5 lg:col-span-4">
                 <div className="grid grid-cols-12 h-20 text-white p-3 mt-1 text-xs">
                     <div className="col-start-2 col-span-10">
                         Adress: ul. Koletek 4/10<br/>
