6d274fe4f97649f00a4f555e9c561dff9e0654b6
bogdan <bogdanarsaman@gmail.com>
2023-12-29 14:39:21 +0100

    made gifs smaller

--- monster-studio/app/routes/info.tsx
+++ monster-studio/app/routes/info.tsx
@@ -9,11 +9,11 @@ export default function Info() {
                 <div className="grid grid-cols-2 gap-5">
                     <div>
                         <p className="mb-2 text-lg">Krok 1</p>
-                        <img className="rounded-3xl w-96" src="MonsterP1.gif" alt="this slowpoke moves" />
+                        <img className="rounded-3xl w-96" src="MonsterP1480.gif" alt="this slowpoke moves" />
                     </div>
                     <div>
                         <p className="mb-2 text-lg">Krok 2</p>
-                        <img className="rounded-3xl w-96" src="MonsterP2.gif" alt="this slowpoke moves" />
+                        <img className="rounded-3xl w-96" src="MonsterP2480.gif" alt="this slowpoke moves" />
                     </div>
                     <div className="mt-5 h-96 col-span-2">
                         <iframe className="w-full h-full rounded-xl" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d212.36176321082417!2d19.939238736941796!3d50.0522710708242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b6b8bc5f96f%3A0xc326ae93cfe14e78!2sKoletek%204%2C%2031-069%20Krak%C3%B3w!5e0!3m2!1sru!2spl!4v1703853598818!5m2!1sru!2spl" frameBorder="0" allowFullScreen={true} loading="lazy">
