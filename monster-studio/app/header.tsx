ff2a7b41a8c46466cc08708aa38740d867e210ae
bogdan <bogdanarsaman@gmail.com>
2024-01-04 22:46:04 +0100

    change header tabs naming

--- monster-studio/app/header.tsx
+++ monster-studio/app/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
             <Link to=""><img src="Monster-studio-logo.png" className="w-24 py-3" alt="Monster studio logo" /></Link>
             <ul className='flex justify-center rounded-3xl bg-white navbar'>
                 <Link to=""><li className='hover:bg-gray-200 rounded-3xl'>Głowna</li></Link>
-                <Link to="cennik"><li className='hover:bg-gray-200 rounded-3xl'>Cennik + Zapis</li></Link>
+                <Link to="cennik"><li className='hover:bg-gray-200 rounded-3xl'>Zapis</li></Link>
                 <Link to="info"><li className='hover:bg-gray-200 rounded-3xl'>Info</li></Link>
             </ul>
             <a href='https://www.instagram.com/monster_might/' className='w-5 pt-4'><img src="instlg.png" alt="instagram"/></a>
