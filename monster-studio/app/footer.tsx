07db06b2fab39fa28ecddb880a42b1977a51a8ef
bogdan <bogdanarsaman@gmail.com>
2024-01-06 19:29:22 +0100

    made footer without bg and less contrast, added info

--- monster-studio/app/footer.tsx
+++ monster-studio/app/footer.tsx
@@ -1,11 +1,18 @@
 export default function Footer() {
     return (
         <div className="w-full grid grid-cols-12 items-end">
-            <div className="bg-zinc-900 rounded-t-[5rem] col-start-2 col-span-10 lg:col-start-5 lg:col-span-4">
+            <div className="rounded-t-[5rem] border-t-2 border-neutral-700 col-start-2 col-span-10 lg:col-start-5 lg:col-span-4 mt-3">
                 <div className="grid grid-cols-12 h-20 text-white p-3 mt-1 text-xs">
                     <div className="col-start-2 col-span-10">
-                        Adress: ul. Koletek 4/10<br/>
-                        Email: smthveryreal@gmail.com
+                        <div className="grid grid-cols-2 text-neutral-500">
+                            <div>
+                                Adress: ul. Koletek 4/10
+                            </div>
+                            <div className="text-right">
+                                +48794451272<br/>
+                                monsterstudiopd@gmail.com
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
