fb0c283ba95f00e4a38272cd3d93ebc9e2d915fa
bogdan <bogdanarsaman@gmail.com>
2024-01-06 19:28:53 +0100

    change header from navbar to separate buttons

--- monster-studio/app/header.tsx
+++ monster-studio/app/header.tsx
@@ -4,12 +4,19 @@ export default function Header() {
     return (
         <div className='grid justify-items-center'>
             <Link to=""><img src="Monster-studio-logo.png" className="w-24 py-3" alt="Monster studio logo" /></Link>
-            <ul className='flex justify-center rounded-3xl bg-white navbar'>
-                <Link to=""><li className='hover:bg-gray-200 rounded-3xl'>Głowna</li></Link>
-                <Link to="cennik"><li className='hover:bg-gray-200 rounded-3xl'>Zapis</li></Link>
-                <Link to="info"><li className='hover:bg-gray-200 rounded-3xl'>Info</li></Link>
+            <ul className='flex justify-center rounded-3xl navbar gap-3'>
+                <Link to=""><li className='hover:bg-gray-200 rounded-3xl bg-white'>Głowna</li></Link>
+                <Link to="zapisy"><li className='hover:bg-gray-200 rounded-3xl bg-white'>Zapisy</li></Link>
+                <Link to="info"><li className='hover:bg-gray-200 rounded-3xl bg-white'>Info</li></Link>
             </ul>
-            <a href='https://www.instagram.com/monster_might/' className='w-5 pt-4'><img src="instlg.png" alt="instagram"/></a>
+            <div className="flex gap-3">
+                <a href='https://www.instagram.com/monster_might/' className='w-5 pt-4'>
+                    <img src="instlg.png" alt="instagram logo"/>
+                </a>
+                <a href='https://youtube.com/@Darth._.julder' className='w-7 pt-4'>
+                    <img src="youtubelg.png" alt="youtube logo"/>
+                </a>
+            </div>
         </div>
     )
 }
