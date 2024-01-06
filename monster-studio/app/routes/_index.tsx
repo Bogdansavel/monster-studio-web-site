3e795f6e25fe94e417c28a443676d4e5c31d2b5c
bogdan <bogdanarsaman@gmail.com>
2024-01-06 19:29:58 +0100

    add new photo and description to main

--- monster-studio/app/routes/_index.tsx
+++ monster-studio/app/routes/_index.tsx
@@ -17,19 +17,18 @@ export default function Index() {
       <div className="w-96 h-72 lg:h-[25rem] lg:w-[40rem] 2xl:h-[35rem] 2xl:w-[50rem] pt-4">
         <Flowbite theme={{ theme: customTheme }}>
           <Carousel slideInterval={7000}>
-            <img className='rounded-3xl' src="IMG_3522.JPG" alt="..." />
-            <img className='rounded-3xl' src="IMG_3523.JPG" alt="..." />
-            <img className='rounded-3xl' src="IMG_3524.JPG" alt="..." />
+            <img className='rounded-3xl' src="IMG_6029.JPG" alt="..." />
+            <img className='rounded-3xl' src="IMG_6031.JPG" alt="..." />
+            <img className='rounded-3xl' src="IMG_6052.JPG" alt="..." />
           </Carousel>
         </Flowbite>
       </div>
       <div className='grid grid-cols-12'>
           <p className='text-white py-4 col-start-2 col-span-10 2xl:col-start-4 2xl:col-span-6 text-center mt-3'>
-          W Monster studio zajęcia z pole Dance i Exotic trwają 80 minut. Na sali 7 rur i 4 koła na drugiej, będziesz sam na rurce. 
-          Z Monster studio będziesz zawsze wysportowany i rozciągnięty po takich zajęciach:
+            Z Monster studio będziesz zawsze wysportowany i rozciągnięty po takich zajęciach:
           </p>
-          <div className='col-start-2 col-span-10 2xl:col-start-4 2xl:col-span-6'>
-            <div className='grid justify-items-center grid-cols-3 sm:grid-cols-4'>
+          <div className='col-start-2 col-span-10 2xl:col-start-5 2xl:col-span-4'>
+            <div className='grid justify-items-center grid-cols-3'>
               <Activity img="stretch.png" name='Stretching' 
               desc='Zajęcia rozciągające polegające na poprawieniu zakresu ruchu danej partii ciała. 
               Na zajęcia weź ze soba top i leginsy.'/>
@@ -56,6 +55,9 @@ export default function Index() {
               <Activity img="pole art.png" name='Pole Art'
               desc='To taniec na rurce z elementami wykonywanymi również obok niej.
               Na zajęcia weź ze sobą nakolanniki oraz top z szortami.'/>
+              <Activity img="aerial silks.png" name='Aerial Silks'
+              desc='To dyscyplina sportowa, która polega na wykonywaniu figur i kombinacji 
+              na podwieszonej do sufitu szarfie. Proszę ubrać coś z długim  rękawem.'/>
             </div>
           </div>
         </div>
